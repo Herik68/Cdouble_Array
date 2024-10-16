d536b50f50514a9595246581ab065530df7fba9e
Herik <herik@wyco.com>
2024-10-16 21:52:31 +0700

    visualized

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -15,6 +15,7 @@ int main(){
     
     cout << "Enter a number from 0 to 100: ";
     cin >> key;
+    cout<<endl;
     result = binarySearch(x, xsize, key);
     if(result!=-1){
         cout << "\n" << key << " is found at index " << result << endl;
@@ -28,14 +29,24 @@ int binarySearch(int a[], const int asize, int key){
     int middle, low=0, high=asize-1;
     while(low<=high){
         middle = (high+low)/2;
+        cout<<"Middlenum= "<< a[middle] <<" and its index = "<< middle <<endl;
         if(key==a[middle]){
             return middle;
         }else if(key<a[middle]){
             high = middle - 1;
+            cout<<"Key("<<key<<")<Middle("<<a[middle]<<")\nSearch the Left Side\nThe array was reduced to index "<< low << " - " << high << endl;
+            for(int i=low;i<=high;i++){
+                cout<< a[i]<< " ";
+            }cout<<endl<<endl;
         }else{
             low = middle + 1;
+            cout<<"Key("<<key<<")>Middle("<<a[middle]<<")\nThe array was reduced to index "<< low << " - " << high << endl;
+            for(int i=low;i<=high;i++){
+                cout<< a[i]<< " ";
+            }cout<<endl<<endl;
         }
     }
+    cout<<"Low greater than High and go outside the loop founding nothing.";
     return -1;
 }
  
